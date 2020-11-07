09b15feeb0266cea265ca92e41f16efc90452f95
gitgitWi <johnwi@knou.ac.kr>
2020-11-07 15:47:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# seoul-nari
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/seoul-nari)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Seoul Nari</h1>
+    </div>
+  );
+}
