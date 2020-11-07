b460fc41bb90e67b3a45a25b767fe35e3151618e
gitgitWi <johnwi@knou.ac.kr>
2020-11-07 20:06:33 +0900

    FEAT: MainSearchBar styling

--- src/components/MainSearchBar.jsx
+++ src/components/MainSearchBar.jsx
@@ -10,23 +10,45 @@ const StyledSearchBar = styled.div`
   width: 60vw;
   max-width: 900px;
   height: 3.5rem;
-  background-color: rgb(255, 209, 102);
-`;
 
-const StyledSearchIcon = styled.div`
-  width: 35px;
+  margin: 15px;
+  box-shadow: 0px 0px 10px rgb(88, 53, 94);
+  border-radius: 15px;
+
+  &:before {
+    width: 35px;
+    padding: 0px 10px;
+    border-right: 1px solid rgb(88, 53, 94);
+
+    text-align: center;
+    content: "🔍";
+  }
 `;
 
 const StyledSearchInput = styled.input.attrs({
   placeholder: "나리 맛집 검색, 렛츠기릿~"
 })`
   width: 100%;
+  height: 2rem;
+
+  border: 0;
+
+  font-family: "Noto Sans KR", sans-serif;
+  text-align: center;
+  font-style: italic;
+
+  &:hover {
+    cursor: grab;
+  }
+
+  &:focus {
+    outline: none;
+  }
 `;
 
-export default function StyledSearchBar() {
+export default function MainSearchBar() {
   return (
     <StyledSearchBar>
-      <StyledSearchIcon />
       <StyledSearchInput />
     </StyledSearchBar>
   );
